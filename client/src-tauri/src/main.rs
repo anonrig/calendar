332e96222d779b24d543aec368119e5849b72dd2
Yagiz Nizipli <yagiz@nizipli.com>
2023-08-24 13:35:30 -0400

    feat: persist window state

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -2,16 +2,32 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::{Manager, SystemTrayEvent, WindowEvent};
+use tauri_plugin_window_state::{AppHandleExt, StateFlags, WindowExt};
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            if let Some(window) = app.get_window("main") {
+                // Restore window size/state.
+                window.restore_state(StateFlags::all())?;
+            }
+            Ok(())
+        })
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .on_window_event(|event| {
-            if let WindowEvent::CloseRequested { api, .. } = event.event() {
-                // Hide windows instead of closing on macOS.
-                if cfg!(target_os = "macos") {
-                    api.prevent_close();
-                    event.window().hide().unwrap();
+            match event.event() {
+                WindowEvent::CloseRequested { api, .. } => {
+                    // Hide windows instead of closing on macOS.
+                    if cfg!(target_os = "macos") {
+                        api.prevent_close();
+                        event.window().hide().unwrap();
+                    }
                 }
+                WindowEvent::Resized(_) => {
+                    // Persist window state
+                    event.window().app_handle().save_window_state(StateFlags::all()).unwrap();
+                }
+                _ => {}
             }
         })
         .on_system_tray_event(|app, event| {
