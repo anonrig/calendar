d0ff2127f669ed04da40a44b098a1d0751dbbba8
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2023-08-24 16:02:34 +0200

    run lint-fix

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,17 +1,15 @@
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 import { Inter } from 'next/font/google'
-import {ThemeProvider} from "@/components/theme-provider";
-import {PropsWithChildren} from "react";
+import { PropsWithChildren } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({
-  children,
-}: PropsWithChildren) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           {children}
         </ThemeProvider>
       </body>
