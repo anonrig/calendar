d01a2c0980464fa18e0793d70ab93a7512b95c63
Yagiz Nizipli <yagiz@nizipli.com>
2023-09-02 14:51:46 -0400

    fix: handle linting errors

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -5,12 +5,12 @@ import { UserAuthForm } from '@/components/user-auth-form'
 export default function AuthenticationPage() {
   return (
     <>
-      <div className='container relative hidden min-h-screen min-w-screen flex-col items-center justify-center md:grid lg:px-0'>
-        <div className='lg:p-8'>
-          <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
-            <div className='flex flex-col space-y-2 text-center'>
-              <h1 className='text-2xl font-semibold tracking-tight'>Create an account</h1>
-              <p className='text-sm text-muted-foreground'>
+      <div className="container relative hidden min-h-screen min-w-screen flex-col items-center justify-center md:grid lg:px-0">
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
+              <p className="text-sm text-muted-foreground">
                 Enter your email below to create your account
               </p>
             </div>
